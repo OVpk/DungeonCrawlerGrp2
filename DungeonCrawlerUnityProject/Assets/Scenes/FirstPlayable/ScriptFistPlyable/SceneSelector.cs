5c68d823623048f50f6659e072f404fffa35cccc
BuildTools <unconfigured@null.spigotmc.org>
2025-04-29 03:27:28 +0200

    MainScene add for FirstPlayable

--- DungeonCrawlerUnityProject/Assets/Scenes/FirstPlayable/ScriptFistPlyable/SceneSelector.cs
+++ DungeonCrawlerUnityProject/Assets/Scenes/FirstPlayable/ScriptFistPlyable/SceneSelector.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button selectButton;        // Le bouton du Canvas
+    public string sceneNameToSelect;   // Le nom de la scène à stocker
+    public static string selectedScene; // Variable statique pour stocker la scène choisie
+
+    private void Start()
     {
-        
+        // Quand on clique sur le bouton, on appelle la fonction SelectScene
+        selectButton.onClick.AddListener(SelectScene);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SelectScene()
     {
-        
+        // Stocker la scène choisie dans la variable statique
+        selectedScene = sceneNameToSelect;
+        Debug.Log("Scène sélectionnée : " + selectedScene);
     }
 }
