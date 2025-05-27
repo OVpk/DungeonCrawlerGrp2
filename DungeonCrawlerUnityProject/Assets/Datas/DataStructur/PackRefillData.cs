f6beb946bda939df0003060e3f7a9d85790e8a73
VERFAILLIE <olivier.verfaillie59@laposte.net>
2025-05-27 02:20:52 +0200

    Recharge de paquet

--- DungeonCrawlerUnityProject/Assets/Datas/DataStructur/PackRefillData.cs
+++ DungeonCrawlerUnityProject/Assets/Datas/DataStructur/PackRefillData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Data/Fight/Item/PackRefill")]
 public class PackRefillData : ItemData
 {
     [field: SerializeField] public CandyPackData packToRefill { get; private set; }
