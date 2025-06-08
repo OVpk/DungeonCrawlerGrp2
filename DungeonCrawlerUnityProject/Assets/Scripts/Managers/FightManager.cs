8d59dca1166ca175afe7c55ba73d3016c980eb5d
VERFAILLIE <olivier.verfaillie59@laposte.net>
2025-06-08 16:29:08 +0200

    backgrounds

--- DungeonCrawlerUnityProject/Assets/Scripts/Managers/FightManager.cs
+++ DungeonCrawlerUnityProject/Assets/Scripts/Managers/FightManager.cs
@@ -176,8 +176,18 @@ public class FightManager : MonoBehaviour, IFightDisplayerListener
         LoadFightArea((FightAreaData)ExplorationManager.Instance.currentArea);
     }
 
+    public GameObject backgroundContainer;
+
+    public void InitBackground(GameObject background)
+    {
+        GameObject currentBackground = backgroundContainer.transform.GetChild(0).gameObject;
+        Destroy(currentBackground);
+        Instantiate(background, backgroundContainer.transform);
+    }
+
     public void LoadFightArea(FightAreaData data)
     {
+        InitBackground(data.backgroundInThisFight);
         isGameEnded = false;
         InitPack();
         InitEnemyGrid(data.enemyGrid);
